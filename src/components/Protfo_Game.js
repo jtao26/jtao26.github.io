7de3cedbc8749010ad17207b4d69febc045ae1d6
Todd Tao <jtao26@jhu.edu>
2024-08-15 23:56:04 +0800

    add raytracer

--- src/components/Protfo_Game.js
+++ src/components/Protfo_Game.js
@@ -1,5 +1,5 @@
 import AStar from '../assets/images/a-star-path.png'
-import QYay from '../assets/images/QYay.png'
+import RayTracer from '../assets/images/ray-tracer.png'
 import UTransit from '../assets/images/UTransit.png'
 
 const Protfo_Game = () => {
@@ -7,11 +7,52 @@ const Protfo_Game = () => {
         <div className='protfolio-container'>
                 <div className='item item-first'>
                     <div className='image'>
+                        <img src={RayTracer} alt='Project Img' width={300}/>
+                    </div>
+                    <div className='desc'>
+                        <div className='title'>
+                            <div className='text'>
+                                Ray Tracer
+                            </div>
+                            <a href='https://github.com/jtao26/ray-tracer'>
+                                <button className='proj-git-btn'>
+                                    <div className='button-text'>
+                                        Source Code
+                                    </div>
+                                </button>
+                            </a>
+                        </div>
+                        
+                        <div className='lables'>
+                            <button className='btn yellow'> C/C++ </button>
+                            <button className='btn green indent'> Linux </button>
+                            {/* <button className='btn blue indent'> WebSocket </button>
+                            <button className='btn purple indent'> PostgreSQL </button>
+                            <button className='btn blue indent'> Azure </button> */}
+                        </div>
+                        <div className='intro'>
+                            C/C++ based scene renderer that generates images of arbitrary 
+                            prefabricated objects in space using Phong Illumination Model, 
+                            with support for textures, simulated depth of field, and ray tracing.
+                        </div>
+                    </div>
+                </div>
+                <div className='item item-first'>
+                    <div className='image'>
                         <img src={AStar} alt='Project Img' width={300}/>
                     </div>
                     <div className='desc'>
                         <div className='title'>
-                            Pathfinding 
+                            <div className='text'>
+                                A* Pathfinding
+                            </div>
+                            <a href='https://github.com/jtao26/A-Star-PathFinding'>
+                                <button className='proj-git-btn'>
+                                    <div className='button-text'>
+                                        Source Code
+                                    </div>
+                                </button>
+                            </a>
                         </div>
                         <div className='lables'>
                             <button className='btn yellow'> C# </button>
@@ -32,9 +73,7 @@ const Protfo_Game = () => {
                 
                 
                 <br></br>
-                <div>
-                    More are comming on the way..
-                </div>
+
             </div>
     )
 }
